8ae314f60b4b51397b5f2c0f4d56b236d77bcd49
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-06-05 06:18:50 +0000

    [odd]ruby1-5

---
+++ ruby1/apple.rb
@@ -0,0 +1,10 @@
+apple = "Yamanasi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県さんです"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県さんではなく、長野県産です"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません"
+end  
+    
---
+++ ruby1/weight.rb
@@ -0,0 +1,6 @@
+name = "A"
+wegiht = 50
+
+puts name + "さんの体重は" + wegiht.to_s + "kgです" #この行を追加
+puts "#{name}さんの体重は#{wegiht}kgです" #この行を追加
+puts '#{name}さんの体重は#{wegiht}kgです' #この行を追加
