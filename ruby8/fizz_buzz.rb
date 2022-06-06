ee8f7c52b94be680b7236cade8cb91c72c766387
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-06-06 02:58:38 +0000

    [odd]

--- ruby8/fizz_buzz.rb
+++ ruby8/fizz_buzz.rb
@@ -1,1 +1,21 @@
-puts "数字を入力してください。"
+def fizz_buzz(number)
+  
+  if number % 15 ==0
+    "fizz_buzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number.to_s
+        
+    
+  end
+  
+end
+puts "数字を入力してください"
+
+input = gets.to_i
+
+puts "検索結果は..."
+puts fizz_buzz(input)
