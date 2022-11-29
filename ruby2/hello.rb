e3051572497b6fdd3ef89609dc559a0947d47e17
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-11-29 14:56:15 +0000

    復習

--- ruby2/hello.rb
+++ ruby2/hello.rb
@@ -1,3 +1,4 @@
 puts 'Hello, World!'
+puts "Hello, World!"
 
 
---
+++ ruby2/review.rb
@@ -0,0 +1,11 @@
+puts 5
+puts "5"
+
+puts 5+3
+puts "5+3"
+puts "5" + "3"
+
+puts "I"+"am"+"San"
+
+puts "Sanの年齢は" + 27.to_s + "才です。"
+puts 100 + "200".to_i
