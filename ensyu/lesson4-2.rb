f49df741c6b812f786370d03137dfb191d2f56ac
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-12-02 14:06:29 +0000

    学習

--- ensyu/lesson4-2.rb
+++ ensyu/lesson4-2.rb
@@ -1,3 +1,18 @@
-subjects = ["国語","算数","理科","社会"]
+def fizz_buzz(number)
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 5 == 0
+    "Buzz"
+  elsif number % 3 == 0
+    "Fizz"
+  else
+    number.to_s
+  end  
+end
 
-puts subjects[2]
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts "結果は..."
+puts fizz_buzz(input)
